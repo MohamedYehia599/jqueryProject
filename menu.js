371cadb158433f9a76e44d51385d5d923b22abd6
MohamedYehia599 <mohamed.yehia3331@gmail.com>
2022-04-02 20:25:13 +0200

    release_1.0

---
+++ menu.js
@@ -0,0 +1,101 @@
+const items = {
+    "categories": [{
+        "section": "appetizers",
+        "id": "0",
+        "menu-items": [{
+          "name": "chicken-wings",
+          "id": "01",
+          "description": "Tender, Spicy and Juicy. Original or Peri-Crusted",
+          "image": "photos/chicken_wings.jpeg",
+          "price": "100"
+        }, {
+          "name": "hot-nachos",
+          "id": "02",
+          "description": " Spicy and yummy nachos",
+          "image": "photos/hot_nachos.jpeg",
+          "price": "80"
+        }]
+      },
+      {
+        "section": "pizza",
+        "id": "1",
+        "menu-items": [{
+          "name": "chicken-pizza",
+          "id": "11",
+          "description": " pizza with chicken and cheese  ",
+          "image": "photos/chicken_pizza.jpeg",
+          "price": "150"
+        }, {
+          "name": "salami-pizza",
+          "id": "12",
+          "description": " pizza with salami and cheese",
+          "image": "photos/salami_pizza.jpeg",
+          "price": "180"
+        }]
+      },
+      {
+        "section": "pasta",
+        "id": "2",
+        "menu-items": [{
+          "name": "chicken-alfredo",
+          "id": "21",
+          "description": " pasta with chicken and cheese  ",
+          "image": "photos/chicken_alfredo.jpeg",
+          "price": "110"
+        }, {
+          "name": "oven-baked-pasta",
+          "id": "22",
+          "description": " pasta baked in oven ",
+          "image": "photos/oven_baked_pasta.jpeg",
+          "price": "90"
+        }]
+      }, {
+        "section": "burgers",
+        "id": "3",
+        "menu-items": [{
+          "name": "cheese-burger",
+          "id": "31",
+          "description": " beef burger with cheddar cheese and our special souce ",
+          "image": "photos/cheese_burger.jpeg",
+          "price": "120"
+        }, {
+          "name": "big-chick",
+          "id": "32",
+          "description": " fried chicken burger with bacon and our special souce  ",
+          "image": "photos/big_chick.jpeg",
+          "price": "100"
+        }]
+      }, {
+        "section": "beverages",
+        "id": "4",
+        "menu-items": [{
+            "name": "turkish-coffee",
+            "id": "41",
+            "description": " special turkish coffee ",
+            "image": "photos/turkish_coffee.jpeg",
+            "price": "25"
+          }, {
+            "name": "french-coffee",
+            "id": "42",
+            "description": " turkish coffee with milk ",
+            "image": "photos/french_coffee.jpeg",
+            "price": "35"
+          },
+          {
+            "name": "orange-juice",
+            "id": "43",
+            "description": " fresh orange juice sugar free ",
+            "image": "photos/orange_juice.jpeg",
+            "price": "40"
+          },
+          {
+            "name": "mango-juice",
+            "id": "43",
+            "description": " fresh mango juice sugar free ",
+            "image": "photos/mango_juice.jpeg",
+            "price": "40"
+          }
+        ]
+      }
+    ]
+}
